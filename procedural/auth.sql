89fa0c62d4de8f2a1455fa6dbe5479b1b9b78a92
pj8912 <59218902+pj8912@users.noreply.github.com>
2021-04-12 18:43:30 +0530

    Create auth.sql
    database: users table

---
+++ procedural/auth.sql
@@ -0,0 +1,7 @@
+create table users(
+user_id int auto_increment primary key not null,
+user_fullname varchar(300) not null,
+user_email varchar(300) not null,
+user_uname varchar(300) not null,
+user_pwd varchar(300) not null
+);

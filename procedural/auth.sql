9ec264b39a40f5d957d2f99fe2f46e94f03fc637
pj8912 <jpintofun2b@gmail.com>
2021-07-20 11:42:13 +0530

    deleted

--- procedural/auth.sql
+++
@@ -1,7 +0,0 @@
-create table users(
-user_id int auto_increment primary key not null,
-user_fullname varchar(300) not null,
-user_email varchar(300) not null,
-user_uname varchar(300) not null,
-user_pwd varchar(300) not null
-);
